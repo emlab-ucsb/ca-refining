a22878020136e738b7b9b12e9d256ba5d3cc0702
Chris Malloy <cmalloy@ucsb.edu>
2024-06-19 14:04:23 -0500

    updated labor check with census regions

--- before-targets/exploratory/labor_check.R
+++ before-targets/exploratory/labor_check.R
@@ -1,3 +1,4 @@
+
 ###### CHECK LABOR OUTPUTS
 
 rm(list=ls())
@@ -25,7 +26,7 @@ library(janitor)
 
 
 #setwd('C:/Users/mall0065/Dropbox/calepa/refining-labor')
-setwd('~/Downloads')
+setwd('~/Dropbox/calepa/refining-labor')
 
 ### define function for "not in" 
 '%!in%' <- function(x,y)!('%in%'(x,y))
@@ -36,12 +37,12 @@ letters_only <- function(x) !grepl("[^A-Za-z*-]", x)
 
 # READ IN LABOR MULTIPLIERS 
 
-multipliers <- fread('20240524-1million_la-Detail Economic Indicators.csv') 
+multipliers <- fread('20240605-census_regions-Detail Economic Indicators.csv') 
 
 multipliers <- janitor::clean_names(multipliers)
 
 multipliers <- multipliers[, .(employment = sum(employment),
-             emp_comp = sum(employee_compensation)), .(origin_region, destination_region, impact_type)]
+                               emp_comp = sum(employee_compensation)), .(origin_region, destination_region, impact_type)]
 
 multipliers[, impact_type := tolower(impact_type)]
 
@@ -50,7 +51,7 @@ multipliers[, county := str_remove(origin_region, " County, CA Group")]
 setnames(multipliers, c("origin_region", "destination_region"), c("origin","destination"))
 
 multipliers <- multipliers[, .(county, origin, destination, impact_type, employment, emp_comp)]
-  
+
 str(multipliers)
 
 multipliers %>% 
@@ -63,7 +64,7 @@ multipliers %>%
 
 # READ IN LABOR IMPACTS 
 
-df <- fread('labor_result_for_review.csv')
+df <- fread('labor_result_for_review (2).csv')
 str(df)
 
 ## CALCULATE DIFFERENCE FROM BAU AND AGGREGATE 
@@ -73,7 +74,9 @@ df.agg <- group_by(df,
   summarize(total_comp_usd19_h = sum(total_comp_usd19_h,na.rm = TRUE),
             total_comp_usd19_l =  sum(total_comp_usd19_l,na.rm = TRUE),
             total_emp = sum(total_emp,na.rm = TRUE),
-            total_emp_revised =  sum(total_emp_revised,na.rm = TRUE)) %>% 
+            total_emp_revised =  sum(total_emp_revised,na.rm = TRUE),
+            total_revenue = sum(total_revenue,na.rm=TRUE),
+            total_production_bbl = sum(total_production_bbl,na.rm=TRUE)) %>% 
   ungroup()
 summary(df.agg$total_comp_usd19_h)
 summary(df.agg$total_comp_usd19_l)
@@ -82,16 +85,38 @@ summary(df.agg$total_emp_revised)
 
 
 df.bau <- filter(df,
-                 demand_scenario=="BAU" & refining_scenario=="historic exports") %>% 
+                 demand_scenario=="BAU" & refining_scenario=="historic production") %>% 
   group_by(demand_scenario,refining_scenario,year) %>% 
   summarize(total_comp_usd19_h = sum(total_comp_usd19_h,na.rm = TRUE),
             total_comp_usd19_l =  sum(total_comp_usd19_l,na.rm = TRUE),
             total_emp = sum(total_emp,na.rm = TRUE),
-            total_emp_revised =  sum(total_emp_revised,na.rm = TRUE)) %>% 
+            total_emp_revised =  sum(total_emp_revised,na.rm = TRUE),
+            total_revenue = sum(total_revenue,na.rm=TRUE),
+            total_production_bbl = sum(total_production_bbl,na.rm=TRUE)) %>% 
   rename(total_comp_usd19_h_bau = total_comp_usd19_h,
          total_comp_usd19_l_bau = total_comp_usd19_l,
          total_emp_bau = total_emp,
-         total_emp_revised_bau = total_emp_revised) %>% 
+         total_emp_revised_bau = total_emp_revised,
+         total_revenue_bau = total_revenue,
+         total_production_bbl_bau = total_production_bbl) %>% 
+  ungroup() %>% 
+  dplyr::select(-demand_scenario,-refining_scenario)
+
+df.county.bau <- filter(df,
+                        demand_scenario=="BAU" & refining_scenario=="historic production") %>% 
+  group_by(demand_scenario,refining_scenario,year,destination) %>% 
+  summarize(total_comp_usd19_h = sum(total_comp_usd19_h,na.rm = TRUE),
+            total_comp_usd19_l =  sum(total_comp_usd19_l,na.rm = TRUE),
+            total_emp = sum(total_emp,na.rm = TRUE),
+            total_emp_revised =  sum(total_emp_revised,na.rm = TRUE),
+            total_revenue = sum(total_revenue,na.rm=TRUE),
+            total_production_bbl = sum(total_production_bbl,na.rm=TRUE)) %>% 
+  rename(total_comp_usd19_h_bau = total_comp_usd19_h,
+         total_comp_usd19_l_bau = total_comp_usd19_l,
+         total_emp_bau = total_emp,
+         total_emp_revised_bau = total_emp_revised,
+         total_revenue_bau = total_revenue,
+         total_production_bbl_bau = total_production_bbl) %>% 
   ungroup() %>% 
   dplyr::select(-demand_scenario,-refining_scenario)
 
@@ -106,18 +131,22 @@ summary(df.agg$total_comp_usd19_h)
 summary(df.agg$total_comp_usd19_l)
 summary(df.agg$total_emp)
 summary(df.agg$total_emp_revised)
+summary(df.agg$total_revenue)
+summary(df.agg$total_production_bbl)
 
-df.county <- left_join(df,df.bau,by=c("year")) %>% 
+df.county <- left_join(df,df.county.bau,by=c("destination","year")) %>% 
   mutate(total_comp_usd19_h = total_comp_usd19_h-total_comp_usd19_h_bau,
          total_comp_usd19_l = total_comp_usd19_l-total_comp_usd19_l_bau,
          total_emp = total_emp-total_emp_bau,
-         total_emp_revised = total_emp_revised-total_emp_revised_bau)
+         total_emp_revised = total_emp_revised-total_emp_revised_bau,
+         total_revenue_gap = total_revenue-total_revenue_bau,
+         total_production_bbl_gap = total_production_bbl - total_production_bbl_bau)
 
 ### PATHWAYS
 
 #### FIG 1: COMPENSATION LOWER BOUND
 fig1 <- filter(df.agg, 
-               demand_scenario=="BAU" & refining_scenario=="low exports" & year>2020) %>%
+               demand_scenario=="LC1" & refining_scenario=="low exports") %>%
   ggplot(aes(y=total_comp_usd19_l/1000, x=year)) + 
   geom_line(size=1, color="#841617") +
   theme_cowplot(12) +
@@ -153,7 +182,7 @@ fig2
 
 #### FIG 3: FTE JOBS 
 fig3 <- filter(df.agg, 
-               demand_scenario=="LC1" & refining_scenario=="historic exports" & year>2020) %>%
+               demand_scenario=="LC1" & refining_scenario=="low exports") %>%
   ggplot(aes(y=total_emp_revised/1000, x=year)) + 
   geom_line(size=1, color="#841617") +
   theme_cowplot(12) +
@@ -173,7 +202,7 @@ fig3
 ## FIG 4: FTE JOBS BY COUNTY
 
 fig4 <- filter(df.county, 
-               demand_scenario=="LC1" & refining_scenario=="historic exports" & year>2020) %>%
+               demand_scenario=="LC1" & refining_scenario=="low exports") %>%
   ggplot(aes(y=total_emp_revised/1000, x=year, color=destination)) + 
   geom_line(size=1, aes(color=destination)) +
   theme_cowplot(12) +
@@ -189,13 +218,13 @@ fig4 <- filter(df.county,
         axis.title = element_text(size=14)) 
 fig4
 
-ggsave('emp_check.png',fig4,width = 10,height=5,units = "in",dpi=300)   
+ggsave('emp_check.png',fig4,width = 10,height=5,units = "in",dpi=300,bg="white")   
 
 
 ## FIG 5: Compensation BY COUNTY
 
 fig5 <- filter(df.county, 
-               demand_scenario=="LC1" & refining_scenario=="historic exports" & year>2020) %>%
+               demand_scenario=="LC1" & refining_scenario=="low exports") %>%
   ggplot(aes(y=total_comp_usd19_l/1000000, x=year, color=destination)) + 
   geom_line(size=1, aes(color=destination)) +
   theme_cowplot(12) +
@@ -211,7 +240,7 @@ fig5 <- filter(df.county,
         axis.title = element_text(size=14)) 
 fig5
 
-ggsave('comp_check.png',fig5,width = 10,height=5,units = "in",dpi=300)   
+ggsave('comp_check.png',fig5,width = 10,height=5,units = "in",dpi=300,bg="white")   
 
 
 #### FIG 6: COMPENSATION LOWER BOUND
@@ -234,7 +263,7 @@ fig6
 
 #### FIG 7: FTE JOBS 
 fig7 <- filter(df.county, 
-               demand_scenario=="LC1" & refining_scenario=="historic exports" & year>2022 & destination=="Siskiyou County, CA") %>%
+               demand_scenario=="LC1" & refining_scenario=="historic exports") %>%
   ggplot(aes(y=total_emp_revised/1000, x=year)) + 
   geom_line(size=1, color="#841617") +
   theme_cowplot(12) +
@@ -249,3 +278,83 @@ fig7 <- filter(df.county,
         axis.text = element_text(size=14),
         axis.title = element_text(size=14)) 
 fig7 
+
+
+#### FIG 8: REVENUE
+fig8 <- filter(df.county, 
+               demand_scenario=="LC1" & refining_scenario=="low exports") %>%
+  ggplot(aes(y=total_revenue_gap/10^9, x=year,color=destination)) + 
+  geom_line(size=1, aes(color=destination)) +
+  theme_cowplot(12) +
+  scale_x_continuous(limits = c(2020, 2045)) + 
+  #scale_y_continuous(limits = c(-60,60)) +
+  labs(y="Revenue (Billions of Dollars, Difference with BAU)", x = "Year",color="",linetype="") +
+  theme(panel.background = element_blank(),
+        legend.position = "top", legend.justification = "center",
+        panel.grid.major.y = element_line(color = "gray",size=0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.text = element_text(size=14),
+        axis.title = element_text(size=14)) 
+fig8
+
+ggsave('revenue_check.png',fig8,width = 10,height=5,units = "in",dpi=300,bg="white")   
+
+#### FIG 9: PRODUCTION GAP
+
+fig9 <- filter(df.county, 
+               demand_scenario=="LC1" & refining_scenario=="low exports") %>%
+  ggplot(aes(y=total_production_bbl_gap/10^9, x=year,color=destination)) + 
+  geom_line(size=1, aes(color=destination)) +
+  theme_cowplot(12) +
+  scale_x_continuous(limits = c(2020, 2045)) + 
+  #scale_y_continuous(limits = c(-60,60)) +
+  labs(y="Total Production (Billions of Barrels, Difference with BAU)", x = "Year",color="",linetype="") +
+  theme(panel.background = element_blank(),
+        legend.position = "top", legend.justification = "center",
+        panel.grid.major.y = element_line(color = "gray",size=0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.text = element_text(size=14),
+        axis.title = element_text(size=14)) 
+fig9
+
+#### FIG 10: PRODUCTION LEVEL
+fig10 <- filter(df.agg, 
+                demand_scenario=="LC1" & refining_scenario=="historic exports") %>%
+  ggplot(aes(y=total_production_bbl_bau/10^9, x=year)) + 
+  geom_line(size=1, color="#841617") +
+  geom_line(aes(y=total_production_bbl/10^9),size=1, color="blue") +
+  theme_cowplot(12) +
+  scale_x_continuous(limits = c(2020, 2045)) + 
+  #scale_y_continuous(limits = c(0,20)) +
+  labs(y="Production (Billions of Barrels)", x = "Year",color="",linetype="") +
+  theme(panel.background = element_blank(),
+        legend.position = "top", legend.justification = "center",
+        panel.grid.major.y = element_line(color = "gray",size=0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.text = element_text(size=14),
+        axis.title = element_text(size=14)) 
+fig10
+
+
+#### FIG 11: REVENUE GAP, JUST LA
+fig11 <- filter(df.county, 
+                demand_scenario=="LC1" & refining_scenario=="low exports" & destination=="Los Angeles") %>%
+  ggplot(aes(y=total_revenue/10^9, x=year)) + 
+  geom_line(size=1, color="#841617") +
+  geom_line(aes(y=total_revenue_bau/10^9),size=1, color="blue") +
+  theme_cowplot(12) +
+  scale_x_continuous(limits = c(2020, 2045)) + 
+  #scale_y_continuous(limits = c(-60,60)) +
+  labs(y="Revenue (Billions of Dollars)", x = "Year",color="",linetype="") +
+  theme(panel.background = element_blank(),
+        legend.position = "top", legend.justification = "center",
+        panel.grid.major.y = element_line(color = "gray",size=0.5),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.text = element_text(size=14),
+        axis.title = element_text(size=14)) 
+fig11
+
