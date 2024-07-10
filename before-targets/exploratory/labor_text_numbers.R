75e03b91e7caf94cf91cd83a50fbee091fe44900
Malloy <christopher.malloy@ou.edu>
2024-07-10 12:57:18 -0500

    updated to use new file tracey made

--- before-targets/exploratory/labor_text_numbers.R
+++ before-targets/exploratory/labor_text_numbers.R
@@ -25,8 +25,8 @@ library(cowplot)
 library(janitor)
 
 
-#setwd('C:/Users/mall0065/Dropbox/calepa/refining-labor')
-setwd('~/Library/CloudStorage/GoogleDrive-cmalloy@ucsb.edu/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/academic-out/refining/figures')
+setwd('G:/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/academic-out/refining/figures')
+#setwd('~/Library/CloudStorage/GoogleDrive-cmalloy@ucsb.edu/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/academic-out/refining/figures')
 
 ### define function for "not in" 
 '%!in%' <- function(x,y)!('%in%'(x,y))
@@ -36,7 +36,61 @@ letters_only <- function(x) !grepl("[^A-Za-z*-]", x)
 
 # IMPORT DISAGGREGATED RESULTS 
 
-df <- fread('2022-12-update/fig-csv-files/state_labor_levels_fig_gaps_pmil_inputs.csv')
+df <- fread('2022-12-update/fig-csv-files/labor_high_low_annual_outputs.csv') 
 str(df)
+summary(df$sum_demo_emp)
+summary(df$sum_demo_emp_revised)
 
-df.2020 <- filter(df, year==2020)
+# CALCULATE PERCENT REDUCTION RELATIVE TO BAU 
+
+## AGGREGATE IMPACTS ACROSS DEMOGRAPHIC GROUPS AND YEARS, SELECT 1 DEMOGRAPHIC CATEGORY SO IMPACTS ARENT DUPLICATED
+
+df.agg <- filter(df,
+                 demo_cat=="DAC") %>% 
+  group_by(scenario,year) %>% 
+  summarize(sum_demo_emp = sum(sum_demo_emp),
+            sum_demo_emp_revised = sum(sum_demo_emp_revised),
+            sum_demo_comp_pv_h = sum(sum_demo_comp_pv_h),
+            sum_demo_comp_pv_l = sum(sum_demo_comp_pv_l)) %>% 
+  ungroup()
+
+## SUBTRACT BAU FROM SCENARIO IMPACT AND DIVIDE BY BAU FOR PERCENT REDUCTION 
+
+df.lc1 <- filter(df.agg,
+                 scenario=="Low demand - low exports")
+
+df.bau <- filter(df.agg,
+                 scenario=="BAU demand - historical production") %>% 
+  rename(sum_demo_emp_bau = sum_demo_emp,
+         sum_demo_emp_revised_bau = sum_demo_emp_revised,
+         sum_demo_comp_pv_h_bau = sum_demo_comp_pv_h,
+         sum_demo_comp_pv_l_bau = sum_demo_comp_pv_l) %>% 
+  dplyr::select(-scenario)
+
+df.agg <- filter(df.agg,
+                 scenario != "BAU demand - historical production") %>% 
+  left_join(df.bau, by="year") %>% 
+  group_by(scenario) %>% 
+  summarize(sum_demo_emp = sum(sum_demo_emp),
+            sum_demo_emp_revised = sum(sum_demo_emp_revised),
+            sum_demo_comp_pv_h = sum(sum_demo_comp_pv_h),
+            sum_demo_comp_pv_l = sum(sum_demo_comp_pv_l),
+            sum_demo_emp_bau = sum(sum_demo_emp_bau),
+            sum_demo_emp_revised_bau = sum(sum_demo_emp_revised_bau),
+            sum_demo_comp_pv_h_bau = sum(sum_demo_comp_pv_h_bau),
+            sum_demo_comp_pv_l_bau = sum(sum_demo_comp_pv_l_bau)) %>%
+  ungroup() %>%
+  mutate(perc_emp = sum_demo_emp/sum_demo_emp_bau,
+         perc_emp_revised = sum_demo_emp_revised/sum_demo_emp_revised_bau,
+         perc_comp_h = sum_demo_comp_pv_h/sum_demo_comp_pv_h_bau,
+         perc_comp_l = sum_demo_comp_pv_l/sum_demo_comp_pv_l_bau,
+         gap_emp = sum_demo_emp-sum_demo_emp_bau,
+         gap_emp_revised = sum_demo_emp_revised-sum_demo_emp_revised_bau,
+         gap_comp_h = sum_demo_comp_pv_h-sum_demo_comp_pv_h_bau,
+         gap_comp_l = sum_demo_comp_pv_l-sum_demo_comp_pv_l_bau)
+
+summary(df.agg$perc_emp)
+summary(df.agg$perc_emp_revised)
+summary(df.agg$perc_comp_h)
+summary(df.agg$perc_comp_l)
+
