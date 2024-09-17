0219622b91abdbd30ec9bff4c207573694ce3047
vthivierge <vthivierge@ucsb.edu>
2024-09-17 16:19:03 -0400

    cleaning files

--- before-targets/exploratory/cumlutative_number_mortality.R
+++
@@ -1,9 +0,0 @@
-### Cumulative mortality
-
-cum_m <- fread("cumulative_avoided_mortality.csv"  , stringsAsFactors = F) ; cum_m
-
-cum_m%>%
-  filter(demo_cat %in% c("Race"))%>%
-  group_by(scen_id)%>%
-  summarise(cumul_mort_level = sum(cumul_mort_level ))
-filter(scen_id %in% c("BAU historical production","LC1 historical exports","LC1 low exports", "BAU historical exports"))
