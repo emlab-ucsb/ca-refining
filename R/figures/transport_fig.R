4ba1768b556291383c7c422e32102f92f2505b7a
Measrainsey Meng <measrainsey@pm.me>
2024-10-01 12:38:46 +0200

    Run `styler` (`tidyverse_style`) on all .R files in directory

--- R/figures/transport_fig.R
+++ R/figures/transport_fig.R
@@ -19,90 +19,92 @@ create_srm_xwalk <- function(main_path,
                              ct_xwalk,
                              raw_counties,
                              raw_ct_2020_all) {
-  
   srm_pm25_df <- copy(srm_weighted_pm25)
   srm_pm25_df[, GEOID := paste0("0", GEOID)]
-  
+
   srm_pm25_df[, weighted_total_pm25 := weighted_totalpm25_nh3 + weighted_totalpm25_nox + weighted_totalpm25_pm25 +
-                weighted_totalpm25_sox + weighted_totalpm25_voc]
-  
+    weighted_totalpm25_sox + weighted_totalpm25_voc]
+
   ## Adjust mismatch of census tract ids between inmap and benmap (census ID changed in 2012
   ## http://www.diversitydatakids.org/sites/default/files/2020-02/ddk_coi2.0_technical_documentation_20200212.pdf)
   srm_pm25_df[, GEOID := ifelse(GEOID == "06037137000", "06037930401", GEOID)]
-  
+
   ## select relevant columns from xwalk
   ct_xwalk_df <- copy(ct_xwalk)
-    
+
   ## select relevant columns from xwalk
   setDT(ct_xwalk_df)
   ct_xwalk_df <- ct_xwalk_df[, .(GEOID_2020, GEOID_2019, rel_intersect)]
-  
+
   ## prepare pollution output
   setnames(srm_pm25_df, "GEOID", "GEOID_2019")
-  
+
   ## merge
-  srm_pm25_df = merge(srm_pm25_df, ct_xwalk, 
-                          by = c("GEOID_2019"),
-                          all = TRUE,
-                          allow.cartesian = TRUE)
-  
+  srm_pm25_df <- merge(srm_pm25_df, ct_xwalk,
+    by = c("GEOID_2019"),
+    all = TRUE,
+    allow.cartesian = TRUE
+  )
+
   ## calculate pm2.5 for 2020 census tract, weight by rel_intersection
-  srm_pm25_df <- srm_pm25_df[, .(weighted_total_pm25 = weighted.mean(weighted_total_pm25, rel_intersect, na.rm = T)), 
-                                     by = .(site_id, GEOID_2020)]
-  
+  srm_pm25_df <- srm_pm25_df[, .(weighted_total_pm25 = weighted.mean(weighted_total_pm25, rel_intersect, na.rm = T)),
+    by = .(site_id, GEOID_2020)
+  ]
+
   ## filter out NA GEOID_2020, NA pm2.5
   srm_pm25_df <- srm_pm25_df[!is.na(GEOID_2020)]
-  
+
   ## rename columns
-  setnames(srm_pm25_df, 
-           c("GEOID_2020", "weighted_total_pm25"), 
-           c("census_tract", "total_pm25"))
-  
+  setnames(
+    srm_pm25_df,
+    c("GEOID_2020", "weighted_total_pm25"),
+    c("census_tract", "total_pm25")
+  )
+
   ## county names
-  county_names <- raw_counties %>% 
-    select(COUNTYFP, NAME) %>% 
+  county_names <- raw_counties %>%
+    select(COUNTYFP, NAME) %>%
     st_drop_geometry() %>%
     unique()
-  
-  ## geoid to census tract 
-  county_df <- raw_ct_2020_all %>% 
+
+  ## geoid to census tract
+  county_df <- raw_ct_2020_all %>%
     filter(STATEFP == "06") %>%
     select(census_tract = GEOID, COUNTYFP, ALAND) %>%
     st_drop_geometry() %>%
     left_join(county_names) %>%
     select(census_tract, COUNTYFP, NAME)
-  
+
   ## add counties to srm
-  srm_pm25_df = merge(srm_pm25_df, county_df, 
-                      by = c("census_tract"),
-                      all = TRUE,
-                      allow.cartesian = TRUE)
-  
-  
+  srm_pm25_df <- merge(srm_pm25_df, county_df,
+    by = c("census_tract"),
+    all = TRUE,
+    allow.cartesian = TRUE
+  )
+
+
   ## save pm2.5 exposure by refinery
   fwrite(srm_pm25_df, file.path(main_path, "outputs/academic-out/refining/figures/2024-08-update/fig-csv-files/", "srm_pm25_refinery_level.csv"))
-  #fwrite(srm_pm25_df, file.path(main_path, "outputs/academic-out/refining/figures/2024-08-beta-adj/fig-csv-files/", "srm_pm25_refinery_level.csv"))
-  
-  
+  # fwrite(srm_pm25_df, file.path(main_path, "outputs/academic-out/refining/figures/2024-08-beta-adj/fig-csv-files/", "srm_pm25_refinery_level.csv"))
+
+
   return(srm_pm25_df)
-  
 }
 
 create_srm_ct <- function(main_path,
                           refinery_pm25_srm) {
-  
   pm25_srm <- copy(refinery_pm25_srm)
-  
-  pm25_srm <- pm25_srm[, .(total_pm25 = sum(total_pm25)), 
-                      by = .(census_tract, COUNTYFP, NAME)]
-  
+
+  pm25_srm <- pm25_srm[, .(total_pm25 = sum(total_pm25)),
+    by = .(census_tract, COUNTYFP, NAME)
+  ]
+
   ## save pm2.5 exposure for each ct
   fwrite(pm25_srm, file.path(main_path, "outputs/academic-out/refining/figures/2024-08-update/fig-csv-files/", "srm_pm25_ct.csv"))
-  #fwrite(pm25_srm, file.path(main_path, "outputs/academic-out/refining/figures/2024-08-beta-adj/fig-csv-files/", "srm_pm25_ct.csv"))
-  
-  
+  # fwrite(pm25_srm, file.path(main_path, "outputs/academic-out/refining/figures/2024-08-beta-adj/fig-csv-files/", "srm_pm25_ct.csv"))
+
+
   return(pm25_srm)
-  
 }
 
 create_pulse_fig <- function(main_path,
@@ -112,69 +114,71 @@ create_pulse_fig <- function(main_path,
                              raw_ct_2020_all,
                              refin_locs,
                              ca_crs) {
-
   ## Refineries plus
   refin_new_locations <- copy(refin_locs)
-  
+
   ## join census tract sp data with ct pm2.5
   ct_pm25_srm_sp <- ct_pm25_srm %>%
-    left_join(raw_ct_2020_all %>% 
-                filter(STATEFP == "06") %>% 
-                select(census_tract = GEOID, geometry)) %>%
+    left_join(raw_ct_2020_all %>%
+      filter(STATEFP == "06") %>%
+      select(census_tract = GEOID, geometry)) %>%
     st_as_sf() %>%
     st_transform(ca_crs)
-  
+
   ## join census tract sp data with refinery level pm2.5 srm
   refinery_pm25_srm_sp <- refinery_pm25_srm %>%
-    left_join(raw_ct_2020_all %>% 
-                filter(STATEFP == "06") %>% 
-                select(census_tract = GEOID, geometry)) %>%
+    left_join(raw_ct_2020_all %>%
+      filter(STATEFP == "06") %>%
+      select(census_tract = GEOID, geometry)) %>%
     filter(!is.na(site_id)) %>%
     st_as_sf() %>%
     st_transform(ca_crs)
-  
+
   ## site ids
   site_ids <- unique(refinery_pm25_srm$site_id)
-  
+
   ## for each site id, plot and put relevant county boundaries
-  
-  for(i in 1:length(site_ids)) {
-    
+
+  for (i in 1:length(site_ids)) {
     id_tmp <- site_ids[i]
-    
+
     srm_tmp <- refinery_pm25_srm_sp %>%
       filter(site_id == id_tmp)
-    
+
     srm_tmp_c <- refinery_pm25_srm_sp %>%
       filter(site_id == id_tmp) %>%
       filter(total_pm25 >= 0.0001)
-    
+
     county_plot_tmp <- srm_tmp_c %>%
       select(COUNTYFP) %>%
       st_drop_geometry() %>%
       unique()
-  
+
     county_tmp <- raw_counties %>%
       filter(COUNTYFP %in% unique(srm_tmp_c$COUNTYFP))
-    
+
     refin_tmp <- refin_new_locations %>%
       filter(site_id == id_tmp)
-    
+
     refin_tmp_name <- as.character(refin_tmp$refinery_name[1])
-    
+
     ## figure
     pm25_fig_tmp <- ggplot() +
-      geom_sf(data = srm_tmp %>% filter(COUNTYFP %in% unique(srm_tmp_c$COUNTYFP)), aes(fill=total_pm25, geometry = geometry), color=NA) +
+      geom_sf(data = srm_tmp %>% filter(COUNTYFP %in% unique(srm_tmp_c$COUNTYFP)), aes(fill = total_pm25, geometry = geometry), color = NA) +
       # theme_void() +
       theme_minimal() +
-      scale_fill_gradient(high = "#A84268", low = "#FAFAFA", space = "Lab", na.value = "grey50",
-                          limits = c(min(srm_tmp$total_pm25), max(srm_tmp$total_pm25)),
-                          breaks = c(0, 0.004)) +
-      labs(title = paste0("PM2.5 concentration from ", refin_tmp_name),
-           y = NULL,
-           x = NULL,
-           # title = bold(expression(bold(paste("PM"[2.5], " concentration from ", refin_tmp_name))),
-           fill=expression(paste("PM"[2.5], " (",mu,"/",m^3,")"))) +
+      scale_fill_gradient(
+        high = "#A84268", low = "#FAFAFA", space = "Lab", na.value = "grey50",
+        limits = c(min(srm_tmp$total_pm25), max(srm_tmp$total_pm25)),
+        breaks = c(0, 0.004)
+      ) +
+      labs(
+        title = paste0("PM2.5 concentration from ", refin_tmp_name),
+        y = NULL,
+        x = NULL,
+        # title = bold(expression(bold(paste("PM"[2.5], " concentration from ", refin_tmp_name))),
+        fill = expression(paste("PM"[2.5], " (", mu, "/", m^3, ")"))
+      ) +
       geom_sf(data = county_tmp, mapping = aes(geometry = geometry), lwd = 0.15, alpha = 0) +
       geom_sf_text(data = county_tmp, aes(label = NAME)) +
       geom_sf(data = refin_tmp, mapping = aes(geometry = geometry), alpha = 0.9, pch = 16) +
@@ -188,12 +192,15 @@ create_pulse_fig <- function(main_path,
         legend.title = element_text(size = 8),
         legend.text = element_text(size = 8),
         plot.margin = margin(0, 2, 0, 8),
-        plot.title = element_text(face = 'bold', size = 8, hjust = -0.05)) +
-      guides(fill = guide_colourbar(title.position="top",
-                                    title.hjust = 0,
-                                    direction = "horizontal",
-                                    ticks.colour = "black", frame.colour = "black",
-                                    order = 1))
+        plot.title = element_text(face = "bold", size = 8, hjust = -0.05)
+      ) +
+      guides(fill = guide_colourbar(
+        title.position = "top",
+        title.hjust = 0,
+        direction = "horizontal",
+        ticks.colour = "black", frame.colour = "black",
+        order = 1
+      ))
 
 
     # ggsave(plot = pm25_fig_tmp,
@@ -203,59 +210,69 @@ create_pulse_fig <- function(main_path,
     #        # width = 6.5,
     #        # height = 8,
     #        dpi = 300)
-    
-    
+
+
     # NOTE from Meas: the plot save directory is hard coded in this function instead of _targets.R, not always easy to track
     # I've added `create.dir = TRUE` otherwise the function will fail if the directory doesn't exist
     # I also added the usage of `file.path` to make it more robust to whether a path has "/" at the end or not
-    ggsave(plot = pm25_fig_tmp,
-           filename = paste0(file.path(main_path, "outputs/academic-out/refining/figures/2024-08-update/pulse-figs", "pulse_"),
-                             id_tmp, ".jpeg"),
-           device = "jpeg",
-           # width = 6.5,
-           # height = 8,
-           dpi = 300,
-           create.dir = TRUE)
-    
+    ggsave(
+      plot = pm25_fig_tmp,
+      filename = paste0(
+        file.path(main_path, "outputs/academic-out/refining/figures/2024-08-update/pulse-figs", "pulse_"),
+        id_tmp, ".jpeg"
+      ),
+      device = "jpeg",
+      # width = 6.5,
+      # height = 8,
+      dpi = 300,
+      create.dir = TRUE
+    )
   }
-  
-## make fig for all locations
-  
+
+  ## make fig for all locations
+
   ## crop
   disp_win2_wgs84 <- st_sfc(st_point(c(-122.5, 33)), st_point(c(-117, 39)),
-                            crs = 4326)
-  
+    crs = 4326
+  )
+
   disp_win2_trans <- st_transform(disp_win2_wgs84, crs = ca_crs)
-  
+
   disp_win2_coord <- st_coordinates(disp_win2_trans)
-  
+
   disp_win_df <- as.data.frame(disp_win2_coord)
-  
+
   ## limits for zoom
   xlim <- c(disp_win_df$X[1], disp_win_df$X[2]) # Set limits for zoom panel
   ylim <- c(disp_win_df$Y[1], disp_win_df$Y[2])
-  
-  
 
-  
+
+
+
   ## plot
-  pm25_fig_all<- ggplot() +
-    geom_sf(data = ct_pm25_srm_sp, aes(fill=total_pm25, geometry = geometry), color=NA) +
+  pm25_fig_all <- ggplot() +
+    geom_sf(data = ct_pm25_srm_sp, aes(fill = total_pm25, geometry = geometry), color = NA) +
     theme_void() +
     # theme_minimal() +
-    scale_fill_gradient(high = "#A84268", low = "#FAFAFA", space = "Lab", na.value = "grey50",
-                        limits = c(min(ct_pm25_srm_sp$total_pm25), max(ct_pm25_srm_sp$total_pm25)),
-                        breaks = c(0.001, 0.02)) +
-    labs(title = "PM2.5 concentration from refineries",
-         y = NULL,
-         x = NULL,
-         # title = bold(expression(bold(paste("PM"[2.5], " concentration from ", refin_tmp_name))),
-         fill=expression(paste("PM"[2.5], " (",mu,"/",m^3,")"))) +
+    scale_fill_gradient(
+      high = "#A84268", low = "#FAFAFA", space = "Lab", na.value = "grey50",
+      limits = c(min(ct_pm25_srm_sp$total_pm25), max(ct_pm25_srm_sp$total_pm25)),
+      breaks = c(0.001, 0.02)
+    ) +
+    labs(
+      title = "PM2.5 concentration from refineries",
+      y = NULL,
+      x = NULL,
+      # title = bold(expression(bold(paste("PM"[2.5], " concentration from ", refin_tmp_name))),
+      fill = expression(paste("PM"[2.5], " (", mu, "/", m^3, ")"))
+    ) +
     geom_sf(data = raw_counties, mapping = aes(geometry = geometry), lwd = 0.15, alpha = 0) +
     # geom_sf_text(data = raw_counties, aes(label = NAME), size = 3) +
     geom_sf(data = refin_new_locations, mapping = aes(geometry = geometry), alpha = 0.8, pch = 16, size = 0.5) +
-    coord_sf(xlim = disp_win2_coord[,'X'], ylim = disp_win2_coord[,'Y'],
-             datum = ca_crs, expand = FALSE) +
+    coord_sf(
+      xlim = disp_win2_coord[, "X"], ylim = disp_win2_coord[, "Y"],
+      datum = ca_crs, expand = FALSE
+    ) +
     theme(
       # legend.justification defines the edge of the legend that the legend.position coordinates refer to
       legend.justification = c(0, 1),
@@ -266,35 +283,38 @@ create_pulse_fig <- function(main_path,
       legend.title = element_text(size = 12),
       legend.text = element_text(size = 12),
       plot.margin = margin(0, 2, 0, 8),
-      plot.title = element_text(face = 'bold', size = 12, hjust = -0.05)) +
-    guides(fill = guide_colourbar(title.position="top",
-                                  title.hjust = 0,
-                                  direction = "horizontal",
-                                  ticks.colour = "black", frame.colour = "black",
-                                  order = 1))
-  
-  # ggsave(plot = pm25_fig_all, 
-  #        filename = paste0(main_path, "outputs/academic-out/refining/figures/2024-08-beta-adj/pulse-figs/pulse_all_crop.jpeg"), 
+      plot.title = element_text(face = "bold", size = 12, hjust = -0.05)
+    ) +
+    guides(fill = guide_colourbar(
+      title.position = "top",
+      title.hjust = 0,
+      direction = "horizontal",
+      ticks.colour = "black", frame.colour = "black",
+      order = 1
+    ))
+
+  # ggsave(plot = pm25_fig_all,
+  #        filename = paste0(main_path, "outputs/academic-out/refining/figures/2024-08-beta-adj/pulse-figs/pulse_all_crop.jpeg"),
   #        device = "jpeg",
   #        # width = 6.5,
   #        # height = 8,
   #        dpi = 300)
-  # 
-  
+  #
+
   # NOTE from Meas: same as above, I switched to using `file.path` instead of paste0 and added `create.dir = TRUE`
-  ggsave(plot = pm25_fig_all,
-         filename = file.path(main_path, "outputs/academic-out/refining/figures/2024-08-update/pulse-figs", "pulse_all_crop.jpeg"),
-         device = "jpeg",
-         # width = 6.5,
-         # height = 8,
-         dpi = 300,
-         create.dir = TRUE)
-  
-  
+  ggsave(
+    plot = pm25_fig_all,
+    filename = file.path(main_path, "outputs/academic-out/refining/figures/2024-08-update/pulse-figs", "pulse_all_crop.jpeg"),
+    device = "jpeg",
+    # width = 6.5,
+    # height = 8,
+    dpi = 300,
+    create.dir = TRUE
+  )
+
+
   return(pm25_fig_all)
-
-
-   }
+}
 
 
 
@@ -303,17 +323,15 @@ create_pulse_fig <- function(main_path,
 # ## -----------------------------------
 # disp_win_la_wgs84 <- st_sfc(st_point(c(-118.5, 33.6)), st_point(c(-117.8, 34.2)),
 #                             crs = 4326)
-# 
+#
 # disp_win_la_trans <- st_transform(disp_win_la_wgs84, crs = ca_crs)
-# 
+#
 # disp_win_la_coord <- st_coordinates(disp_win_la_trans)
-# 
+#
 # zoom_coord_df <- as.data.frame(disp_win_la_coord)
-# 
+#
 # county_crop <- st_crop(CA_counties_noisl, xmin = zoom_coord_df$X[1], xmax = zoom_coord_df$X[2], ymin = zoom_coord_df$Y[1], ymax = zoom_coord_df$Y[2])
 # ct_cropped <- st_crop(ct_map_county, xmin = zoom_coord_df$X[1], xmax = zoom_coord_df$X[2], ymin = zoom_coord_df$Y[1], ymax = zoom_coord_df$Y[2])
-# 
+#
 # ## only include census tracts that are in the crop
 # ct_intersect <- st_intersection(ct_map_county, county_crop)
-
-
--- before-targets/exploratory/pop_share_poverty.R
+++ before-targets/exploratory/pop_share_poverty.R
@@ -1,27 +1,28 @@
-# Goal: Import raw data on population relative to the poverty line from NHGIS 
-rm(list=ls())
+# Goal: Import raw data on population relative to the poverty line from NHGIS
+rm(list = ls())
 
-library(readr) 
+library(readr)
 library(magrittr)
 library(dplyr)
 
-#Set wd 
+# Set wd
 
-path <- '/Users/chrismalloy/Library/CloudStorage/GoogleDrive-cmalloy@ucsb.edu/Shared drives/emlab/projects/current-projects/calepa-cn/data/labor/raw'
+path <- "/Users/chrismalloy/Library/CloudStorage/GoogleDrive-cmalloy@ucsb.edu/Shared drives/emlab/projects/current-projects/calepa-cn/data/labor/raw"
 
 ############################################################################################
 
-setwd(path) 
+setwd(path)
 
-nhgis_df <- read_csv('nhgis0003_csv/nhgis0003_ds254_20215_county.csv', 
-                     col_types = "cccllccccllllllllllllllllllllllllllllcllcciiiiiiiiciiiiiiii") %>% 
-  filter(STATE=="California") %>% 
-  select(TL_GEO_ID,STATE,COUNTY,AOXWE001,AOXWE002,AOXWE003,AOXWE004,AOXWE005,AOXWE006,AOXWE007,AOXWE008) %>% 
+nhgis_df <- read_csv("nhgis0003_csv/nhgis0003_ds254_20215_county.csv",
+  col_types = "cccllccccllllllllllllllllllllllllllllcllcciiiiiiiiciiiiiiii"
+) %>%
+  filter(STATE == "California") %>%
+  select(TL_GEO_ID, STATE, COUNTY, AOXWE001, AOXWE002, AOXWE003, AOXWE004, AOXWE005, AOXWE006, AOXWE007, AOXWE008) %>%
   rename(fips = TL_GEO_ID)
 str(nhgis_df)
 
-## NOTES: 
-### AOXWE001: total population in a county 
+## NOTES:
+### AOXWE001: total population in a county
 ### AOXWE002: population under .50 of the poverty line in CA
 ### AOXWE003: population between .50 and .99 of the poverty line in CA
 ### AOXWE004: population between 1.00 to 1.24 of the poverty line in CA
@@ -30,16 +31,17 @@ str(nhgis_df)
 ### AOXWE007: population between 1.85 to 1.99 of the poverty line in CA
 ### AOXWE008: population between 2.00 and over of the poverty line in CA
 
-# Compute the share of total population in each county that is in each bin relative to the poverty line 
+# Compute the share of total population in each county that is in each bin relative to the poverty line
 
-nhgis_df <- mutate(nhgis_df, 
-                   share_aoxwe002 = AOXWE002/AOXWE001, 
-                   share_aoxwe003 = AOXWE003/AOXWE001,
-                   share_aoxwe004 = AOXWE004/AOXWE001,
-                   share_aoxwe005 = AOXWE005/AOXWE001,
-                   share_aoxwe006 = AOXWE006/AOXWE001,
-                   share_aoxwe007 = AOXWE007/AOXWE001,
-                   share_aoxwe008 = AOXWE008/AOXWE001)
+nhgis_df <- mutate(nhgis_df,
+  share_aoxwe002 = AOXWE002 / AOXWE001,
+  share_aoxwe003 = AOXWE003 / AOXWE001,
+  share_aoxwe004 = AOXWE004 / AOXWE001,
+  share_aoxwe005 = AOXWE005 / AOXWE001,
+  share_aoxwe006 = AOXWE006 / AOXWE001,
+  share_aoxwe007 = AOXWE007 / AOXWE001,
+  share_aoxwe008 = AOXWE008 / AOXWE001
+)
 
 summary(nhgis_df$share_aoxwe002)
 summary(nhgis_df$share_aoxwe003)
@@ -48,6 +50,3 @@ summary(nhgis_df$share_aoxwe005)
 summary(nhgis_df$share_aoxwe006)
 summary(nhgis_df$share_aoxwe007)
 summary(nhgis_df$share_aoxwe008)
-
-
-
