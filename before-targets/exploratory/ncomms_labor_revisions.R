e7278f9b96e3c016503de035e43dcf85095bd5a5
Chris Malloy <cmalloy@ucsb.edu>
2025-02-27 17:21:29 -0600

    added labor script to determine where refinery workers live

---
+++ before-targets/exploratory/ncomms_labor_revisions.R
@@ -0,0 +1,80 @@
+###### NCOMMS REVISIONS 
+
+rm(list = ls())
+
+list.of.packages <- c("dplyr", "data.table", "lubridate", "tidyr", "readxl", "fixest", "modelsummary", "flextable")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
+if (length(new.packages)) install.packages(new.packages)
+
+library(lubridate)
+library(tidyr)
+library(dplyr)
+library(data.table)
+library(readxl)
+library(stringr)
+library(readr)
+library(fixest)
+library(modelsummary)
+library(flextable)
+library(svglite)
+library(sf)
+library(ggplot2)
+library(tigris)
+library(cowplot)
+library(janitor)
+
+
+# setwd('G:/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/academic-out/refining/figures')
+setwd("~/Dropbox/ou/ncomms-revisions")
+
+### define function for "not in"
+"%!in%" <- function(x, y) !("%in%"(x, y))
+
+# Check that string doesn't match any non-letter
+letters_only <- function(x) !grepl("[^A-Za-z*-]", x)
+
+
+# IMPORT LODES DATA 
+
+df <- fread('ca_od_main_JT02_2020.csv') %>%
+  filter(w_geocode == 60379800021006 |
+           w_geocode == 60379800301000 |
+           w_geocode == 60133780001024 |
+           w_geocode == 60952521022025 |
+           w_geocode == 60290024011034 |
+           w_geocode == 60379800141083 |
+           w_geocode == 60379800051007 |
+           w_geocode == 60290005072025 |
+           w_geocode == 60133200011067 |
+           w_geocode == 60133150001075 |
+           w_geocode == 60133580005003 |
+           w_geocode == 60379800151000 |
+           w_geocode == 60790123061025 | 
+           w_geocode == 60375535021000 |
+           w_geocode == 60290005072077) %>%
+  dplyr::select(w_geocode,h_geocode,S000,SI01)
+str(df)
+summary(df$SI01)
+summary(df$S000)
+
+marathon_carson <- filter(df,
+                          w_geocode == 60379800021006) %>%
+  summarize(SI01 = sum(SI01), 
+            S000 = sum(S000))
+summary(marathon_carson$SI01)
+summary(marathon_carson$S000)
+
+chevron_elsegundo <- filter(df,
+                              w_geocode == 60379800301000) %>%
+  summarize(SI01 = sum(SI01), 
+            S000 = sum(S000))
+summary(chevron_elsegundo$SI01)
+summary(chevron_elsegundo$S000)
+
+valero_benicia <- filter(df,
+                            w_geocode == 60952521022025) %>%
+  summarize(SI01 = sum(SI01), 
+            S000 = sum(S000))
+summary(valero_benicia$SI01)
+summary(valero_benicia$S000)
+
