9bfa599b7c01d55c95d2c8fc85ab3dfff3edd340
Chris Malloy <cmalloy@ucsb.edu>
2025-02-28 10:27:35 -0600

    updated with refinery names

--- before-targets/exploratory/ncomms_labor_revisions.R
+++ before-targets/exploratory/ncomms_labor_revisions.R
@@ -49,32 +49,35 @@ df <- fread('ca_od_main_JT02_2020.csv') %>%
            w_geocode == 60133150001075 |
            w_geocode == 60133580005003 |
            w_geocode == 60379800151000 |
-           w_geocode == 60790123061025 | 
+           w_geocode == 60790123061000 | 
            w_geocode == 60375535021000 |
            w_geocode == 60290005072077) %>%
-  dplyr::select(w_geocode,h_geocode,S000,SI01)
+  dplyr::select(w_geocode,h_geocode,S000,SI01) %>% 
+  mutate(refinery_name = ifelse(w_geocode == 60379800021006,"Marathon Carson",
+                                ifelse(w_geocode == 60379800301000,"Chevron El Segundo",
+                                       ifelse(w_geocode == 60133780001024,"Chevron Richmond",
+                                              ifelse(w_geocode == 60952521022025,"Valero Benicia",
+                                                     ifelse(w_geocode == 60290024011034,"Kern Oil Bakersfield",
+                                                            ifelse(w_geocode == 60379800141083,"Valero Wilmington",
+                                                                   ifelse(w_geocode == 60379800051007,"PBF Torrance",
+                                                                          ifelse(w_geocode == 60290005072025,"San Joaquin Bakersfield",
+                                                                                 ifelse(w_geocode == 60133200011067,"PBF Martinez",
+                                                                                        ifelse(w_geocode == 60133150001075,"Marathon Golden Eagle",
+                                                                                               ifelse(w_geocode == 60133580005003,"Phillips 66 Rodeo",
+                                                                                                      ifelse(w_geocode == 60379800151000,"Phillips 66 Wilmington",
+                                                                                                              ifelse(w_geocode == 60790123061000,"Phillips 66 Santa Maria",
+                                                                                                                     ifelse(w_geocode == 60375535021000,"AltAir Paramount",
+                                                                                                                            ifelse(w_geocode == 60290005072077,"Global Clean Energy",NA))))))))))))))))
 str(df)
 summary(df$SI01)
 summary(df$S000)
 
-marathon_carson <- filter(df,
-                          w_geocode == 60379800021006) %>%
+df.refinery <- group_by(df,refinery_name) %>% 
   summarize(SI01 = sum(SI01), 
-            S000 = sum(S000))
-summary(marathon_carson$SI01)
-summary(marathon_carson$S000)
+            S000 = sum(S000)) %>% 
+  ungroup()
+summary(df.refinery$SI01)
+summary(df.refinery$S000)
 
-chevron_elsegundo <- filter(df,
-                              w_geocode == 60379800301000) %>%
-  summarize(SI01 = sum(SI01), 
-            S000 = sum(S000))
-summary(chevron_elsegundo$SI01)
-summary(chevron_elsegundo$S000)
 
-valero_benicia <- filter(df,
-                            w_geocode == 60952521022025) %>%
-  summarize(SI01 = sum(SI01), 
-            S000 = sum(S000))
-summary(valero_benicia$SI01)
-summary(valero_benicia$S000)
 
