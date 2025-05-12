6e20e24ce950881e1f73b8eea3fb5d5515b8a354
Measrainsey Meng <measrainsey@pm.me>
2025-05-12 14:44:19 -0700

    Run formatter

--- R/read_files.R
+++ R/read_files.R
@@ -2,39 +2,96 @@ simple_fread <- function(file) {
   fread(file, header = T)
 }
 
-simple_read_xlsx <- function(file, input_sheet, input_rows = NULL, input_cols = NULL) {
-  setDT(read.xlsx(file, sheet = input_sheet, rows = input_rows, cols = input_cols))
+simple_read_xlsx <- function(
+  file,
+  input_sheet,
+  input_rows = NULL,
+  input_cols = NULL
+) {
+  setDT(read.xlsx(
+    file,
+    sheet = input_sheet,
+    rows = input_rows,
+    cols = input_cols
+  ))
 }
 
 read_refcap_data <- function(file) {
   dt <- simple_fread(file)
-  dt <- dt[, c("site_id", "refinery_name", "barrels_per_day", "location", "county", "cluster")]
+  dt <- dt[, c(
+    "site_id",
+    "refinery_name",
+    "barrels_per_day",
+    "location",
+    "county",
+    "cluster"
+  )]
   setnames(dt, "cluster", "region")
 
   # remove asphalt and non-transportation refineries
-  dt <- dt[!refinery_name %in% c("Greka Energy, Santa Maria Refinery", "Lunday Thagard, South Gate Refinery", "Valero Wilmington Asphalt Refinery")]
+  dt <- dt[
+    !refinery_name %in%
+      c(
+        "Greka Energy, Santa Maria Refinery",
+        "Lunday Thagard, South Gate Refinery",
+        "Valero Wilmington Asphalt Refinery"
+      )
+  ]
 
   dt
 }
 
 read_ref_ghg_data <- function(file, sel_year) {
   dt <- simple_fread(file)
-  reg <- unique(dt[, .(year, region, region_barrels, region_co2e_kg, region_kgco2e_bbl)])
+  reg <- unique(dt[, .(
+    year,
+    region,
+    region_barrels,
+    region_co2e_kg,
+    region_kgco2e_bbl
+  )])
   year_ghg <- reg[year == sel_year]
   year_ghg
 }
 
-read_rediesel_data <- function(file, input_sheet, input_rows = NULL, input_cols = NULL) {
-  dt <- setDT(read.xlsx(file, sheet = input_sheet, rows = input_rows, cols = input_cols))
-  dt <- melt(dt, id.vars = "X1", variable.name = "year", value.name = "consumption_gal")
+read_rediesel_data <- function(
+  file,
+  input_sheet,
+  input_rows = NULL,
+  input_cols = NULL
+) {
+  dt <- setDT(read.xlsx(
+    file,
+    sheet = input_sheet,
+    rows = input_rows,
+    cols = input_cols
+  ))
+  dt <- melt(
+    dt,
+    id.vars = "X1",
+    variable.name = "year",
+    value.name = "consumption_gal"
+  )
   dt[, year := as.numeric(as.character(year))]
   dt[, fuel := "renewable diesel"]
   dt[, X1 := NULL]
   dt
 }
 
-read_altair_data <- function(file, input_sheet, ei_crude_input, ei_gasoline_input, input_rows = NULL, input_cols = NULL) {
-  dt <- setDT(read.xlsx(file, sheet = input_sheet, rows = input_rows, cols = input_cols))
+read_altair_data <- function(
+  file,
+  input_sheet,
+  ei_crude_input,
+  ei_gasoline_input,
+  input_rows = NULL,
+  input_cols = NULL
+) {
+  dt <- setDT(read.xlsx(
+    file,
+    sheet = input_sheet,
+    rows = input_rows,
+    cols = input_cols
+  ))
   dt <- dt[, .(year, refinery_name, barrels_per_day, location, county, region)]
   dt[, bbl_per_year := barrels_per_day * 365]
   dt[, gal_per_year := bbl_per_year * 42]
@@ -43,15 +100,30 @@ read_altair_data <- function(file, input_sheet, ei_crude_input, ei_gasoline_inpu
   dt
 }
 
-read_raw_its_data <- function(file, input_sheet, input_rows = NULL, input_cols = NULL) {
-  dt <- setDT(read.xlsx(file, sheet = input_sheet, rows = input_rows, cols = input_cols))
+read_raw_its_data <- function(
+  file,
+  input_sheet,
+  input_rows = NULL,
+  input_cols = NULL
+) {
+  dt <- setDT(read.xlsx(
+    file,
+    sheet = input_sheet,
+    rows = input_rows,
+    cols = input_cols
+  ))
   colnames(dt) <- c("fuel", "units", 2017:2050)
   dt[, fuel := tolower(fuel)]
   dt
 }
 
 read_raw_fpm_data <- function(file, input_sheet, start_row_input) {
-  dt <- setDT(read.xlsx(file, sheet = input_sheet, startRow = start_row_input, detectDates = T))
+  dt <- setDT(read.xlsx(
+    file,
+    sheet = input_sheet,
+    startRow = start_row_input,
+    detectDates = T
+  ))
   colnames(dt)[2] <- "code"
   dt
 }
@@ -60,7 +132,12 @@ read_raw_ces_data <- function(file) {
   setDT(read_xlsx(file))
 }
 
-read_raw_dac_data <- function(file, input_sheet, input_rows = NULL, input_cols) {
+read_raw_dac_data <- function(
+  file,
+  input_sheet,
+  input_rows = NULL,
+  input_cols
+) {
   dt <- setDT(read.xlsx(file, sheet = input_sheet, cols = input_cols))
   dt <- janitor::clean_names(dt)
   setDT(dt)
@@ -95,11 +172,31 @@ read_ghg_2019_data <- function(file) {
 }
 
 read_inmap_data <- function(inmap_path, bsite) {
-  nh3 <- fread(paste0(inmap_path, "/nh3/srm_nh3_site", bsite, ".csv"), header = TRUE, colClasses = c(GEOID = "character"))
-  nox <- fread(paste0(inmap_path, "/nox/srm_nox_site", bsite, ".csv"), header = TRUE, colClasses = c(GEOID = "character"))
-  pm25 <- fread(paste0(inmap_path, "/pm25/srm_pm25_site", bsite, ".csv"), header = TRUE, colClasses = c(GEOID = "character"))
-  sox <- fread(paste0(inmap_path, "/sox/srm_sox_site", bsite, ".csv"), header = TRUE, colClasses = c(GEOID = "character"))
-  voc <- fread(paste0(inmap_path, "/voc/srm_voc_site", bsite, ".csv"), header = TRUE, colClasses = c(GEOID = "character"))
+  nh3 <- fread(
+    paste0(inmap_path, "/nh3/srm_nh3_site", bsite, ".csv"),
+    header = TRUE,
+    colClasses = c(GEOID = "character")
+  )
+  nox <- fread(
+    paste0(inmap_path, "/nox/srm_nox_site", bsite, ".csv"),
+    header = TRUE,
+    colClasses = c(GEOID = "character")
+  )
+  pm25 <- fread(
+    paste0(inmap_path, "/pm25/srm_pm25_site", bsite, ".csv"),
+    header = TRUE,
+    colClasses = c(GEOID = "character")
+  )
+  sox <- fread(
+    paste0(inmap_path, "/sox/srm_sox_site", bsite, ".csv"),
+    header = TRUE,
+    colClasses = c(GEOID = "character")
+  )
+  voc <- fread(
+    paste0(inmap_path, "/voc/srm_voc_site", bsite, ".csv"),
+    header = TRUE,
+    colClasses = c(GEOID = "character")
+  )
 
   nh3[, pollutant := "nh3"]
   nox[, pollutant := "nox"]
@@ -151,8 +248,22 @@ read_nhgis_data <- function(file) {
   )]
 
   dt <- dt[, .(
-    gisjoin, state, county, total_pop, hispanic, white, black, aialnative, asian, hawaiian_pacisl,
-    nonh_other, nonh_two_or_more, median_income, geoid, year, state
+    gisjoin,
+    state,
+    county,
+    total_pop,
+    hispanic,
+    white,
+    black,
+    aialnative,
+    asian,
+    hawaiian_pacisl,
+    nonh_other,
+    nonh_two_or_more,
+    median_income,
+    geoid,
+    year,
+    state
   )]
 }
 
@@ -178,8 +289,21 @@ read_nhgis_2021_data <- function(file) {
   )]
 
   dt <- dt[, .(
-    gisjoin, state, county, total_pop, hispanic, white, black, aialnative, asian, hawaiian_pacisl,
-    nonh_other, nonh_two_or_more, median_income, geoid, year
+    gisjoin,
+    state,
+    county,
+    total_pop,
+    hispanic,
+    white,
+    black,
+    aialnative,
+    asian,
+    hawaiian_pacisl,
+    nonh_other,
+    nonh_two_or_more,
+    median_income,
+    geoid,
+    year
   )]
 }
 
@@ -204,11 +328,22 @@ read_census_race_data <- function(file) {
   )]
 
   dt <- dt[, .(
-    gisjoin, state, county, total_pop, hispanic, white, black, aialnative, asian, hawaiian_pacisl,
-    nonh_other, nonh_two_or_more, geoid, year
+    gisjoin,
+    state,
+    county,
+    total_pop,
+    hispanic,
+    white,
+    black,
+    aialnative,
+    asian,
+    hawaiian_pacisl,
+    nonh_other,
+    nonh_two_or_more,
+    geoid,
+    year
   )]
 }
-
 
 
 read_poverty_data <- function(file) {
@@ -225,7 +360,16 @@ read_poverty_data <- function(file) {
     total_above_poverty = AOQGE003
   )]
 
-  dt <- dt[, .(gisjoin, geoid, state, county, year, total_above_poverty, total_below_poverty, total_pop)]
+  dt <- dt[, .(
+    gisjoin,
+    geoid,
+    state,
+    county,
+    year,
+    total_above_poverty,
+    total_below_poverty,
+    total_pop
+  )]
 }
 
 # read_labor_inputs <- function(file, input_sheet, input_rows=NULL, input_cols=NULL) {
@@ -247,7 +391,6 @@ read_poverty_data <- function(file) {
 #   dt
 # }
 
-
 read_ca_regions <- function(file) {
   dt <- fread(file)
 
@@ -258,12 +401,21 @@ read_ca_regions <- function(file) {
   adj_regions <- c("census_3", "census_4", "census_5", "census_6")
 
   ## adjust names, make regions 8 and 9 Los Angeles and Orange, respectively
-  dt[, region := fifelse(region %in% adj_regions, paste0(region, "_v2"), region)]
+  dt[,
+    region := fifelse(region %in% adj_regions, paste0(region, "_v2"), region)
+  ]
   dt[, region := fifelse(region == "census_8", "Los Angeles", region)]
   dt[, region := fifelse(region == "census_9", "Orange", region)]
 
   ## updated method uses regions and counties
-  labor_counties <- c("Contra Costa", "Kern", "Los Angeles", "Orange", "San Luis Obispo", "Solano")
+  labor_counties <- c(
+    "Contra Costa",
+    "Kern",
+    "Los Angeles",
+    "Orange",
+    "San Luis Obispo",
+    "Solano"
+  )
 
   ## flag if county is stand alone county, filter out, bind to county_pop_df
   dt[, remove := fifelse(county %in% labor_counties, 1, 0)]
@@ -299,10 +451,7 @@ read_labor_inputs <- function(file, fte_file) {
   dt <- fread(file)
 
   ## merge with fte-job-years data
-  dt <- merge(dt, fte_file,
-    by = c("IndustryCode"),
-    all.x = T
-  )
+  dt <- merge(dt, fte_file, by = c("IndustryCode"), all.x = T)
 
   ## clean column names
   dt <- janitor::clean_names(dt)
@@ -311,10 +460,13 @@ read_labor_inputs <- function(file, fte_file) {
   dt[, employment := employment * ft_eper_total_emp]
 
   ## summarize by origin region, destination region, and impcact type
-  dt <- dt[, .(
-    employment = sum(employment),
-    emp_comp = sum(employee_compensation)
-  ), .(origin_region, destination_region, impact_type)]
+  dt <- dt[,
+    .(
+      employment = sum(employment),
+      emp_comp = sum(employee_compensation)
+    ),
+    .(origin_region, destination_region, impact_type)
+  ]
 
   ## lowercase
   dt[, impact_type := tolower(impact_type)]
@@ -325,7 +477,11 @@ read_labor_inputs <- function(file, fte_file) {
   dt[, destination_region := str_trim(destination_region)]
 
   ## rename columns
-  setnames(dt, c("origin_region", "destination_region"), c("origin", "destination"))
+  setnames(
+    dt,
+    c("origin_region", "destination_region"),
+    c("origin", "destination")
+  )
 
   ## select columns
   dt <- dt[, .(county, origin, destination, impact_type, employment, emp_comp)]
@@ -333,21 +489,36 @@ read_labor_inputs <- function(file, fte_file) {
   dt
 }
 
-read_oil_px <- function(file, input_sheet, input_rows = NULL, input_cols = NULL) {
+read_oil_px <- function(
+  file,
+  input_sheet,
+  input_rows = NULL,
+  input_cols = NULL
+) {
   ## oil prices
   oilpx_scens_real <- setDT(read.xlsx(file, sheet = "real", cols = input_cols))
-  colnames(oilpx_scens_real) <- c("year", "reference_case", "high_oil_price", "low_oil_price")
-  oilpx_scens_real <- melt(oilpx_scens_real,
+  colnames(oilpx_scens_real) <- c(
+    "year",
+    "reference_case",
+    "high_oil_price",
+    "low_oil_price"
+  )
+  oilpx_scens_real <- melt(
+    oilpx_scens_real,
     measure.vars = c("reference_case", "high_oil_price", "low_oil_price"),
-    variable.name = "oil_price_scenario", value.name = "oil_price_usd_per_bbl"
+    variable.name = "oil_price_scenario",
+    value.name = "oil_price_usd_per_bbl"
   )
   oilpx_scens_real[, oil_price_scenario := gsub("_", " ", oil_price_scenario)]
-  oilpx_scens_real[, oil_price_scenario := factor(oil_price_scenario, levels = c("reference case", "high oil price", "low oil price"))]
+  oilpx_scens_real[,
+    oil_price_scenario := factor(
+      oil_price_scenario,
+      levels = c("reference case", "high oil price", "low oil price")
+    )
+  ]
 }
 
-read_refin_locs <- function(file_refin_locs,
-                            file_refin_locs_orig,
-                            ca_crs) {
+read_refin_locs <- function(file_refin_locs, file_refin_locs_orig, ca_crs) {
   refin_crs <- st_crs(st_read(file_refin_locs_orig))
 
   ## Refineries plus
@@ -361,8 +532,6 @@ read_refin_locs <- function(file_refin_locs,
     ) %>%
     st_transform(ca_crs)
 }
-
-
 
 # track_files_basic <- function(file) {
 #   data = fread(file, header = T)
