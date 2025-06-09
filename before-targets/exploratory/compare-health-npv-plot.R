ec892d245d1fb337c30f7399ce2166993f542de1
Tracey Mangin <traceymangin@gmail.com>
2025-06-09 15:22:16 -0700

    compare outputs for health

---
+++ before-targets/exploratory/compare-health-npv-plot.R
@@ -0,0 +1,14 @@
+## compare health npv plot outputs
+
+library(data.table)
+library(tidyverse)
+
+main_path <- "/Users/traceymangin/Library/CloudStorage/GoogleDrive-tmangin@ucsb.edu/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/academic-out/refining/figures/"
+
+submission_df <- "2024-08-update/fig-csv-files/state_npv_fig_inputs_health_may2025.csv"
+newer_df <- "2025-health-revisions/fig-csv-files/state_npv_fig_inputs_health.csv"
+
+orig_df <- fread(paste0(main_path, submission_df))
+new_df <- fread(paste0(main_path, newer_df)) |>
+  rename(new_val = value) |>
+  left_join(orig_df)
