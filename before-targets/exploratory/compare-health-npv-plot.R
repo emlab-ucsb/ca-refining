9b6c0e9f7f8ff13a02c95f4480ba9798f89c25b5
Tracey Mangin <traceymangin@gmail.com>
2025-06-09 15:29:32 -0700

    compute diff

--- before-targets/exploratory/compare-health-npv-plot.R
+++ before-targets/exploratory/compare-health-npv-plot.R
@@ -9,6 +9,8 @@ submission_df <- "2024-08-update/fig-csv-files/state_npv_fig_inputs_health_may20
 newer_df <- "2025-health-revisions/fig-csv-files/state_npv_fig_inputs_health.csv"
 
 orig_df <- fread(paste0(main_path, submission_df))
+
 new_df <- fread(paste0(main_path, newer_df)) |>
   rename(new_val = value) |>
-  left_join(orig_df)
+  left_join(orig_df) |>
+  mutate(diff = new_val - value)
