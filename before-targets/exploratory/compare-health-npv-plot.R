ddad2f7fbe9cdb64066eead0154e918388238279
traceybit <traceymangin@gmail.com>
2025-06-11 17:00:33 -0700

    add growing vsl fig

--- before-targets/exploratory/compare-health-npv-plot.R
+++ before-targets/exploratory/compare-health-npv-plot.R
@@ -3,9 +3,9 @@
 library(data.table)
 library(tidyverse)
 
-main_path <- "/Users/traceymangin/Library/CloudStorage/GoogleDrive-tmangin@ucsb.edu/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/academic-out/refining/figures/"
+main_path <- "/Users/tracey/Library/CloudStorage/GoogleDrive-tmangin@ucsb.edu/Shared drives/emlab/projects/current-projects/calepa-cn/outputs/academic-out/refining/figures/"
 
-submission_df <- "2024-08-update/fig-csv-files/state_npv_fig_inputs_health.csv"
+submission_df <- "2024-orig-submission/fig-csv-files/state_npv_fig_inputs_health.csv"
 newer_df <- "2025-health-revisions/fig-csv-files/state_npv_fig_inputs_health.csv"
 
 orig_df <- fread(paste0(main_path, submission_df))
